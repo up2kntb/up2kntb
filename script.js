21d44b05cdc0b3c44da3a5ca2de4df4d1844eedc
up2kntb <up2kntb.inbox@gmail.com>
2025-05-20 11:13:47 +0800

    memperbaiki kode html

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-fetch("https://sheetdb.io/api/v1/jtll0a7sdmup2")
+fetch("https://sheetdb.io/api/v1/smomo1mxyt3q5")
       .then(res => res.json())
       .then(data => {
 
+        //hanya pakai yang ada datanya
+        const filterData = data.filter(item => item.ID);
+
         // Urutkan berdasarkan tanggal (terbaru di atas)
-        data.sort((a, b) => new Date(b.tanggal) - new Date(a.tanggal));
+        filterData.sort((a, b) => new Date(b.Tanggal) - new Date(a.Tanggal));
 
         const container = document.getElementById("news-container");
         data.forEach(item => {
           const card = `
             <div class="release-card">
               <div class = "thumbnail">
-                <img src="${item.thubnail || 'img/note.jpg'}" class="release-thumb" alt="Thumbnail">
+                <img src="${item.Thumbnail || 'img/note.jpg'}" class="release-thumb" alt="Thumbnail">
               </div>
               <div class="release-content">
-                <h5 class="release-title">${item.judul}</h5>
-                <div class="release-meta">${item.tanggal} | Penulis: ${item.penulis}</div>
+                <h5 class="release-title">${item.Judul}</h5>
+                <div class="release-meta">${item.Tanggal} | Penulis: ${item.Penulis}</div>
               </div>
             </div>
           `;
